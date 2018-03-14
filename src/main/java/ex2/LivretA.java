9a61b0bab6d01fb1f28a7e4a65d038589b417e73
victorahs <victorahsene90@gmail.com>
2018-03-14 15:13:41 +0100

    finish exo 2

--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -2,8 +2,28 @@ package ex2;
 
 public class LivretA extends CompteBancaire {
 
-	public LivretA(String type, double solde, double tauxRemuneration) {
-		super(type, solde, 0, tauxRemuneration);
+	/** tauxRemuneration : taux de rémunération dans le cas d'un livret A */
+	private double tauxRemuneration;
+	private static final int DECOUVERT = 0;
+
+	/**
+	 * Ce constructeur est utilisé pour créer un compte de type Livret A
+	 
+	 * @param solde représente le solde du compte
+	 *            
+	 * @param decouvert  représente le découvert autorisé
+	 *           
+	 * @param tauxRemuneration représente le taux de rémunération du livret A
+	 *    
+	 */
+	public LivretA(double solde, double tauxRemuneration) {
+
+		super(solde, DECOUVERT);
+		this.tauxRemuneration = tauxRemuneration;
 	}
 
+	public void appliquerRemuAnnuelle() {
+
+		this.solde = solde + solde * tauxRemuneration / 100;
+	}
 }
